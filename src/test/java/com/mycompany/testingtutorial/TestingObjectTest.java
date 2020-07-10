dabf201b2bf4e164e1292a29f657dd95e1d1ce04
Peter Crusinberry <pcrusinberry@unomaha.edu>
2020-07-09 20:27:59 -0500

    Cleanup

--- src/test/java/com/mycompany/testingtutorial/TestingObjectTest.java
+++ src/test/java/com/mycompany/testingtutorial/TestingObjectTest.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.testingtutorial;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -106,17 +97,4 @@ public class TestingObjectTest {
     }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
